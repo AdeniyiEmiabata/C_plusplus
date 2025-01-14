35a18e7c798c6353791f5f81512d6de7db96da4a
Adeniyi Emiabata <denem@MacBook-Pro.local>
2025-01-14 08:23:41 -0600

    Fully functional, excluding issues with user input functionality in Generate_Player(), Phase/Side of the ball to be added to class

--- Cmake_Tutorial/NFL_Players.hpp
+++ Cmake_Tutorial/NFL_Players.hpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// List of Divisions
 set<string> AFC_East = {"AFC_East","Buffalo Bills", "New York Jets", "New England Patriots", "Miami Dolphins"};
 set<string> AFC_West = {"AFC_West","Kansas City Chiefs", "Las Vegas Raiders", "Denver Broncos", "Los Angeles Chargers"};
 set<string> AFC_North = {"AFC_North","Baltimore Ravens", "Pittsburgh Steelers", "Cincinnati Bengals", "Cleveland Browns"};
@@ -14,7 +15,9 @@ set<string> NFC_West = {"NFC_West","Seattle Seahwaks", "San Francisco 49ers", "A
 set<string> NFC_North = {"NFC_North","Green Bay Packers", "Minnesota Vikings", "Chicago Bears", "Detroit Lions"};
 set<string> NFC_South = {"NFC_South","Tampa Bay Buccaneers", "New Orleans Saints", "Carolina Panthers", "Atlanta Falcons"};
 
-
+// List of Positions (Sides of the Ball)
+set<string> Defense = {"DT", "DE", "CB", "OLB", "LB", "FS", "SS", "Defense"};
+set<string> Offense = {"QB", "LT", "C", "RT", "LG", "RG", "TE", "FB", "WR", "RB", "Offense"};
 class NFL_Players{
 
 
@@ -24,7 +27,7 @@ string Division;
 string Position;
 string College;
 int Years_of_Exp;
-
+// string Phase (to be added)
 public:
 NFL_Players(string Name, string Team, string Division, string Position, string College, int Years_of_Exp):Name(Name), Team(Team), Division(Division), 
 Position(Position), College(College), Years_of_Exp(Years_of_Exp){};
@@ -47,7 +50,7 @@ int Calculate_Player_Relationship_Score (NFL_Players& PlayerA_Attributes, NFL_Pl
         }
     }
 
-    if (PlayerA_Attributes.Division == PlayerB_Attributes.Division){
+    else if (PlayerA_Attributes.Division == PlayerB_Attributes.Division){
         Score += 50;
         if(PlayerA_Attributes.Position == PlayerB_Attributes.Position){
             Score += 10;
@@ -60,7 +63,7 @@ int Calculate_Player_Relationship_Score (NFL_Players& PlayerA_Attributes, NFL_Pl
         }
     }
 
-    if(PlayerA_Attributes.College == PlayerB_Attributes.College){
+    else if(PlayerA_Attributes.College == PlayerB_Attributes.College){
         Score+= 40;
         if(PlayerA_Attributes.Position == PlayerB_Attributes.Position){
             Score += 10;
@@ -70,14 +73,14 @@ int Calculate_Player_Relationship_Score (NFL_Players& PlayerA_Attributes, NFL_Pl
         }
     }
 
-    if(PlayerA_Attributes.Position == PlayerB_Attributes.Position){
+    else if(PlayerA_Attributes.Position == PlayerB_Attributes.Position){
         Score += 30;
         if (PlayerA_Attributes.Years_of_Exp == PlayerB_Attributes.Years_of_Exp){
                 Score += 10;
             }
     }
 
-    if(PlayerA_Attributes.Years_of_Exp == PlayerB_Attributes.Years_of_Exp){
+    else if(PlayerA_Attributes.Years_of_Exp == PlayerB_Attributes.Years_of_Exp){
         Score += 20;
     }
 
@@ -90,45 +93,50 @@ void Relationship_Remarks(NFL_Players& PlayerA_Attributes, NFL_Players& PlayerB_
 
     int Relationship_Score = Calculate_Player_Relationship_Score(PlayerA_Attributes, PlayerB_Attributes);
 
-    if (Relationship_Score > 20 && Relationship_Score < 45){
+    if (Relationship_Score >= 20 && Relationship_Score < 45){
         cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have a low chemistry score of " << Relationship_Score << "\n";
         cout << "We do not recommend having these players in the same team. Refresh the pool for better options!" << "\n";
         cout << "Low relationship scores affect player performance!" << "\n";
+        return;
     }
 
-    else if (Relationship_Score > 45 && Relationship_Score < 65){
+    else if (Relationship_Score >= 45 && Relationship_Score < 65){
         cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have a below average chemistry score of " << Relationship_Score << "\n";
         cout << "These players are compatible. Reinforce your team with other players with higher relationship scores" << "\n";
         cout << "Low relationship scores affect player performance!" << "\n";
+        return;
     }
 
-    else if (Relationship_Score > 65 && Relationship_Score < 75){
+    else if (Relationship_Score >= 65 && Relationship_Score < 75){
         cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have an average chemistry score of " << Relationship_Score << "\n";
         cout << "These players are mildly compatible. Reinforce your team with other players with higher relationship scores" << "\n";
+        return;
     }
 
-    else if (Relationship_Score > 75 && Relationship_Score < 85){
+    else if (Relationship_Score >= 75 && Relationship_Score < 85){
         cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have a good chemistry score of " << Relationship_Score << "\n";
         cout << "These players are very compatible. Reinforce your team with other players with higher relationship scores" << "\n";
         cout << "High relationship scores improve player performance" << "\n";
+        return;
     }
 
-    else if (Relationship_Score > 85 && Relationship_Score < 100){
+    else if (Relationship_Score >= 85 && Relationship_Score < 100){
         cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have a great chemistry score of " << Relationship_Score << "\n";
         cout << "These players will gel very well. Search the player pool for more players with similar relationship scores" << "\n";
         cout << "High relationship scores improve player performance" << "\n";
+        return;
     }
 
     else if(Relationship_Score == 100){
         cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have an excellent chemistry score of " << Relationship_Score << "\n";
         cout << "Excellent selections! Search the player pool for more players with similar relationship scores" << "\n";
         cout << "High relationship scores improve player performance" << "\n";
+        return;
     }
 
     cout << PlayerA_Attributes.Name << " and " << PlayerB_Attributes.Name << " have an extremely low chemistry score of " << Relationship_Score << "\n";
-        cout << "Avoid putting these players in the same lineup!" << "\n";
-        cout << "Low relationship scores improve player performance" << "\n";
-
+    cout << "Avoid putting these players in the same lineup!" << "\n";
+    cout << "Low relationship scores improve player performance" << "\n";
     return;
 }
 };
@@ -183,7 +191,7 @@ string Search_Division(string Team_Name){
         return Division;
     }
 
-    Division = "Team Not Found! Check your spelling and/or letter casing";
+    Division = "Team Not Found! Check your spelling and/or letter casing\n\n";
     return Division;
 
 }
@@ -196,26 +204,32 @@ NFL_Players Generate_Player(){
     string College;
     int Years_of_Exp;
 
-    cout << "Be mindful of letter casing when entering details!" << "\n";
+    cout << "\n\n------Hit Enter to Start!-------";
+
+    //to ensure the last cin>> for Years_of_Exp does not affect the first getline for the 2nd player getline(cin, Name): Needs Fixing
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    cout << "\nNOTE: Be mindful of letter casing when entering details!" << "\n";
 
    
-    cout << "Input Player Name: " << "\n";
-     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
+    cout << "\n\nInput Player Name: " << "\n";
     getline(cin, Name);
 
-    cout << "Input Player Team: " << "\n";
+    
+
+    cout << "\n\nInput Player Team: " << "\n";
     getline(cin, Team);
 
-    cout << "Input Player Position: " << "\n";
+    cout << "\n\nInput Player Position: " << "\n";
     getline(cin, Position);
 
-    cout << "Input Player College: " << "\n";
+    cout << "\n\nInput Player College: " << "\n";
     getline(cin, College);
 
-    cout << "Input Player's Years of Experience: " << "\n";
+    cout << "\n\nInput Player's Years of Experience: " << "\n";
     cin >> Years_of_Exp;
 
+    cout << "\n\n";
     Division = Search_Division(Team);
     NFL_Players Player_Details(Name, Team, Division, Position, College, Years_of_Exp);
     return Player_Details;
