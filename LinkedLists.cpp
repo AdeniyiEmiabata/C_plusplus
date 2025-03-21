7235c2908d4cea99868f54c4afdbc1c59a87ce1b
Adeniyi Emiabata <denem@MacBook-Pro.local>
2025-03-21 08:18:58 -0500

    added insertatfront, insertsomewhere

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -32,6 +32,51 @@ void Insert_Element(Node*& head, int val){
     
 }
 
+Node* InsertAtFront(Node*& head, int val){
+    if (head == nullptr){
+        head ->next = nullptr;
+        head -> value = val;
+        return head;
+    }
+    Node* new_head = new Node();
+    new_head -> next = head;
+    new_head -> value = val;
+
+    return new_head;
+}
+
+void InsertSomewhere(Node*&head, int val, int n){    // n is how many nodes after the head
+
+    if (head == nullptr){
+        head ->next = nullptr;
+        head -> value = val;
+        return;
+    }
+
+    Node* new_node = new Node();
+    new_node -> value = val;
+
+    int count = 0;
+
+    Node* temp = head;
+    Node* prev = nullptr;
+    while(temp != nullptr){
+        if(count == n){
+            break;
+        }
+        else{
+            count++;
+            prev = temp;
+            temp = temp -> next;
+        }        
+    }
+
+    new_node -> next = temp;
+    prev -> next = new_node;
+    //while(head!= nu)
+
+}
+
 void DisplayElements(Node *head){
     Node* temp = new Node();
     temp = head;
@@ -42,7 +87,7 @@ void DisplayElements(Node *head){
     }
 }
 
-Node* ReverseElements(Node *head){
+Node* ReverseElements(Node*& head){
     Node* curr = head;
     Node* prev = nullptr;
     Node* next_node = nullptr;
@@ -69,6 +114,8 @@ int main(){
     Insert_Element(head, 45);
     Insert_Element(head, 55);
     Insert_Element(head, 65);
+    head = InsertAtFront(head, 199);
+    InsertSomewhere(head, 73, 2);
     std::cout << "OLD LIST: \n"; 
     DisplayElements(head);
     head = ReverseElements(head);
